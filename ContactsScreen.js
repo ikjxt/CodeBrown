73f59b9a92d3a0081d05b76f1ec5b2cd3cb758f5
ikjotshergill <ikjotshergill@csus.edu>
2024-02-25 21:29:42 -0800

    Chat/Contacts UI update
    Chat screen and contact screen user interface improved and new format for adding contacts. So far this is just the user interface side, but will be easy to integrate once the back end is setup on Firebase for messaging.

--- ContactsScreen.js
+++ ContactsScreen.js
@@ -1,16 +1,31 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
-const ContactsScreen = ({ navigation }) => {
-  const handleChatPress = () => {
-    navigation.navigate("ChatScreen");
+const ContactScreen = ({ navigation }) => {
+  const contacts = [
+    { id: '1', name: 'Driver 1' },
+    { id: '2', name: 'Customer 7' },
+    { id: '3', name: 'Manager' },
+  ];
+
+  const handleContactPress = (contact) => {
+    navigation.navigate('ChatScreen', { contactName: contact.name });
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Contacts</Text>
-      {/* Display list of contacts here. Link with firebase. */}
-      <Button title="Chat with User" onPress={handleChatPress} />
+      <FlatList
+        data={contacts}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.contactItem}
+            onPress={() => handleContactPress(item)}>
+            <Text style={styles.contactName}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
@@ -18,15 +33,26 @@ const ContactsScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     paddingTop: 50,
+    paddingLeft: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 20,
+  },
+  contactItem: {
+    backgroundColor: 'red',
+    padding: 20,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  contactName: {
+    fontSize: 18,
+    color: 'white',
   },
 });
 
-export default ContactsScreen;
+export default ContactScreen;
