f3f6fbddcd76ec798345d984d9f68b61a0757e98
Akashdeep Vasisht <105088023+akashdeep42@users.noreply.github.com>
2024-03-30 19:37:31 -0700

    Forgot to add back comments, and updated comments

--- EditProfileScreen.js
+++ EditProfileScreen.js
@@ -7,28 +7,30 @@ import { db } from "./firebaseConfig";
 import { PropTypes } from 'prop-types';
 
 const EditProfileScreen = ({ navigation }) => {
-  const [newFirstName, setNewFirstName] = useState('');
-  const [newLastName, setNewLastName] = useState('');
-  const [newPhoneNumber, setNewPhoneNumber] = useState('');
+  const [newFirstName, setNewFirstName] = useState('');      // For new name from user input
+  const [newLastName, setNewLastName] = useState('');        // For new name from user input
+  const [newPhoneNumber, setNewPhoneNumber] = useState('');  // For new phone number from user input
 
-  const auth = getAuth();
-  const user = auth.currentUser;
-  const documentID = user.email;
-
+  const auth = getAuth();         // Set observer on Auth object,
+  const user = auth.currentUser;  // Get the current user's profile,
+  const documentID = user.email;  // Get the user's email, which is the ID of the document on our Firestore
+ 
+  // User has entered a new first name
   const updateFirstName = async () => {
     try {
-      const userDocRef = doc(db, "USERS", documentID);
+      const userDocRef = doc(db, "USERS", documentID)  // Get the document, this is currently just for "driver"s
       await updateDoc(userDocRef, {
         firstName: newFirstName
       });
     } catch (error) {
       console.error('Error updating first name', error);
+      console.log(documentID);
     }
   };
-
+  // User has entered a new last name
   const updateLastName = async () => {
     try {
-      const userDocRef = doc(db, "USERS", documentID);
+      const userDocRef = doc(db, "USERS", documentID)  // Get the document, this is currently just for "driver"s
       await updateDoc(userDocRef, {
         lastName: newLastName
       });
@@ -36,10 +38,10 @@ const EditProfileScreen = ({ navigation }) => {
       console.error('Error updating last name', error);
     }
   };
-
+  // User has entered a new phone number
   const updatePhoneNumber = async () => {
     try {
-      const userDocRef = doc(db, "USERS", documentID);
+      const userDocRef = doc(db, "USERS", documentID)  // Get the document, this is currently just for "driver"s
       await updateDoc(userDocRef, {
         phoneNumber: newPhoneNumber
       });
@@ -48,74 +50,78 @@ const EditProfileScreen = ({ navigation }) => {
     }
   };
 
+  // Called when user presses the "Submit Changes" button
+  // Changes are only made if user inputs text into text box before pressing button
   const handleSubmitChanges = () => {
-    if (newFirstName || newLastName || newPhoneNumber) {
+    if (newFirstName != '' || newLastName != '' || newPhoneNumber != '') {
       Alert.alert("Changes Saved");
     }
-    if (newFirstName) {
-      updateFirstName();
-      setNewFirstName('');
+    if (newFirstName != '') {  // If a new first name is entered
+      updateFirstName();   
+      setNewFirstName('');  // Clear the Text Box    
     }
-    if (newLastName) {
-      updateLastName();
-      setNewLastName('');
+    if (newLastName != '') {  // If a new last name is entered
+      updateLastName();  
+      setNewLastName('');  // Clear the Text Box    
     }
-    if (newPhoneNumber) {
-      updatePhoneNumber();
-      setNewPhoneNumber('');
+    if (newPhoneNumber != '') {  // If a new phone number is entered
+      updatePhoneNumber(); 
+      setNewPhoneNumber('');  // Clear the Text Box     
     }
   };
 
   const handleChangeEmail = () => {
-    navigation.navigate('Enter Current Password');
-  };
+    navigation.navigate('Enter Current Password ');
+  }
 
   const handleChangePassword = () => {
     navigation.navigate('Enter Current Password');
-  };
+  }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Edit Profile</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="First Name"
-          placeholderTextColor="#666"
-          onChangeText={setNewFirstName}
-          value={newFirstName}
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Last Name"
-          placeholderTextColor="#666"
-          onChangeText={setNewLastName}
-          value={newLastName}
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Phone Number"
-          placeholderTextColor="#666"
-          onChangeText={(text) => setNewPhoneNumber(text.replace(/[^0-9]/g, '').slice(0, 10))}
-          value={newPhoneNumber}
-          keyboardType="numeric"
-          maxLength={10}
-        />
+    <View style={styles.container}> 
+      <Text style={styles.title}>Edit Profile</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="First Name"
+        placeholderTextColor="#666"
+        onChangeText={setNewFirstName}
+        value={newFirstName}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Last Name"
+        placeholderTextColor="#666"
+        onChangeText={setNewLastName}
+        value={newLastName}
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Phone Number"
+        placeholderTextColor="#666"
+        onChangeText={(text) => setNewPhoneNumber(text.replace(/[^0-9]/g, '').slice(0, 10))}
+        value={newPhoneNumber}
+        keyboardType="numeric"
+        maxLength={10}
+      />
 
-        <TouchableOpacity style={styles.button2} onPress={handleChangeEmail}>
-          <Text>Email</Text>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.button2} onPress={handleChangeEmail}>
+        <Text>Email</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button2} onPress={handleChangePassword}>
-          <Text>Password</Text>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.button2} onPress={handleChangePassword}>
+        <Text>Password</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button1} onPress={handleSubmitChanges}>
-          <Text>Submit Changes</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity style={styles.button1} onPress={handleSubmitChanges}>
+        <Text>Submit Changes</Text>
+      </TouchableOpacity>
+
+      
+    </View>
     </TouchableWithoutFeedback>
   );
 };
@@ -127,9 +133,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
-    fontSize: 30,
+    fontSize:30,
     fontWeight: 'bold',
-    textAlign: 'center',
+    textAlign:'center',  // Center the title 
     lineHeight: 60
   },
   input: {
